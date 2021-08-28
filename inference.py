a698bd6fe538a00fe990105879428a9672c3941b
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2021-08-29 03:36:13 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,16 +1,4 @@
-from utils import Zero_Shot_TC
-
-def binary_score(score,label1,label2):
-    text=''
-    if score > 50:
-        text = f'강한 {label1}'
-    elif score>0:
-        text = f'약한 {label1}'
-    elif -50<score<0:
-        text = f'약한 {label2}'
-    elif score<-50:
-        text = f'강한 {label2}'
-    return text
+from utils import Zero_Shot_TC,binary_score
 
 def Risk_Tolerance(text,return_logits=False):
     result = Zero_Shot_TC(text,['위험 회피형','위험 추구형'],"이 문장은 {label} 성격을 가진다.")
@@ -18,3 +6,21 @@ def Risk_Tolerance(text,return_logits=False):
     if return_logits:
         return binary_score(risk_score,'위험 추구형','위험 회피형'),risk_score
     return binary_score(risk_score,'위험 추구형','위험 회피형')
+
+def Financial_Understanding(text,labels,tokenizer,model,return_logits=False):
+    weight = torch.load('/content/drive/MyDrive/KB_NLP/model_2_0.pth',map_location=torch.device('cpu')) #2_0
+    model.load_state_dict(weight)
+    result = {}
+    hypothesis = [f'해당 문장은 {label}에 대해 설명하고 있다.' for label in labels]
+    for label,h in zip(labels,hypothesis):
+        x = tokenizer.encode(text, h, max_length=64, return_tensors='pt',truncation=True)
+        logits = model(x).logits
+        probs = logits.softmax(dim=1)
+        entailment = probs.detach()[:,0]
+        contradiction = probs.detach()[:,2]
+        result[label] = entailment
+    output = dict(zip(result.values(),result.keys()))
+    m = max(output.keys())
+    if return_logits:
+        return output[m] , contradiction
+    return f'해당 문장은 {output[m]}에 대한 이해를 보여주고 있다.'
